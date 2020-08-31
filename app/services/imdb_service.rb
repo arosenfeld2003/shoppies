3e2af879ed99c44121140184c216783da10b5177
Alex Rosenfeld <arosenfeld2003@mac.com>
2020-08-31 10:42:01 -0700

    test hardcode API key into ImdbService for Heroku

--- app/services/imdb_service.rb
+++ app/services/imdb_service.rb
@@ -1,14 +1,16 @@
 class ImdbService
 
   def get_movies_by_title_fragment(str)
-    request = "http://www.omdbapi.com/?apikey=#{ENV["OMDB_API_KEY"]}&s=#{str}"
+    # request = "http://www.omdbapi.com/?apikey=#{ENV["OMDB_API_KEY"]}&s=#{str}"
+    request = "http://www.omdbapi.com/?apikey=ce31ab29&s=#{str}"
     response = HTTParty.get("#{request}").parsed_response
     return response
   end
 
 
   def get_movie_by_title(title)
-    request = "http://www.omdbapi.com/?apikey=#{ENV["OMDB_API_KEY"]}&t=#{title}"
+    # request = "http://www.omdbapi.com/?apikey=#{ENV["OMDB_API_KEY"]}&t=#{title}"
+    "http://www.omdbapi.com/?apikey=ce31ab29&t=#{str}"
     response = HTTParty.get("#{request}").parsed_response
     return response
   end
