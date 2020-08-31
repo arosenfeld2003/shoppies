debcf3f1af3f1d5056bfa907e1f642e4e726e0fb
Alex Rosenfeld <arosenfeld2003@mac.com>
2020-08-31 16:08:43 -0700

    fix bug declare regular variables not ruby ENV variables

--- app/services/imdb_service.rb
+++ app/services/imdb_service.rb
@@ -1,9 +1,9 @@
 class ImdbService
   def get_movies_by_title_fragment(str)
     # set on heroku CLI: heroku config:set OMDB_API_KEY='insert_key_here'
-    OMDB_API_KEY = ENV['OMDB_API_KEY']
+    omd_api_key = ENV['OMDB_API_KEY']
     #LOCAL: request = "http://www.omdbapi.com/?apikey=#{ENV["OMDB_API_KEY"]}&s=#{str}"
-    request = "http://www.omdbapi.com/?apikey=#{OMDB_API_KEY}&s=#{str}"
+    request = "http://www.omdbapi.com/?apikey=#{omd_api_key}&s=#{str}"
     response = HTTParty.get("#{request}").parsed_response
     return response
   end
@@ -11,8 +11,8 @@ class ImdbService
 
   def get_movie_by_title(title)
     # set on heroku CLI: heroku config:set OMDB_API_KEY='insert_key_here'
-    OMDB_API_KEY = ENV['OMDB_API_KEY']
-    request = "http://www.omdbapi.com/?apikey=#{OMDB_API_KEY}&t=#{title}"
+    omd_api_key = ENV['OMDB_API_KEY']
+    request = "http://www.omdbapi.com/?apikey=#{omd_api_key}&t=#{title}"
     response = HTTParty.get("#{request}").parsed_response
     return response
   end
