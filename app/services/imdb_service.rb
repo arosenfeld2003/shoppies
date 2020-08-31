31ba95cdbc1843dee68153df090387f515e62492
Alex Rosenfeld <arosenfeld2003@mac.com>
2020-08-31 15:51:05 -0700

    set API key to heroku ENV variable

--- app/services/imdb_service.rb
+++ app/services/imdb_service.rb
@@ -1,18 +1,18 @@
 class ImdbService
+  # set on heroku CLI: heroku config:set OMDB_API_KEY='insert_key_here'
+  OMDB_API_KEY = ENV['OMDB_API_KEY']
 
   def get_movies_by_title_fragment(str)
-    # request = "http://www.omdbapi.com/?apikey=#{ENV["OMDB_API_KEY"]}&s=#{str}"
-    request = "http://www.omdbapi.com/?apikey=ce31ab29&s=#{str}"
+    #LOCAL: request = "http://www.omdbapi.com/?apikey=#{ENV["OMDB_API_KEY"]}&s=#{str}"
+    request = "http://www.omdbapi.com/?apikey=#{OMDB_API_KEY}&s=#{str}"
     response = HTTParty.get("#{request}").parsed_response
     return response
   end
 
 
   def get_movie_by_title(title)
-    # request = "http://www.omdbapi.com/?apikey=#{ENV["OMDB_API_KEY"]}&t=#{title}"
-    request = "http://www.omdbapi.com/?apikey=ce31ab29&t=#{title}"
+    request = "http://www.omdbapi.com/?apikey=#{OMDB_API_KEY}&t=#{str}"
     response = HTTParty.get("#{request}").parsed_response
     return response
   end
-
 end
