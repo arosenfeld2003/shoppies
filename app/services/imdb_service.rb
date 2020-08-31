49324867f8c16bbf7c34767e200f8a8191b2349a
Alex Rosenfeld <arosenfeld2003@mac.com>
2020-08-31 15:57:08 -0700

    move ENV declaration inside scope of IMDB service methods

--- app/services/imdb_service.rb
+++ app/services/imdb_service.rb
@@ -1,8 +1,7 @@
 class ImdbService
-  # set on heroku CLI: heroku config:set OMDB_API_KEY='insert_key_here'
-  OMDB_API_KEY = ENV['OMDB_API_KEY']
-
   def get_movies_by_title_fragment(str)
+    # set on heroku CLI: heroku config:set OMDB_API_KEY='insert_key_here'
+    OMDB_API_KEY = ENV['OMDB_API_KEY']
     #LOCAL: request = "http://www.omdbapi.com/?apikey=#{ENV["OMDB_API_KEY"]}&s=#{str}"
     request = "http://www.omdbapi.com/?apikey=#{OMDB_API_KEY}&s=#{str}"
     response = HTTParty.get("#{request}").parsed_response
@@ -11,6 +10,8 @@ class ImdbService
 
 
   def get_movie_by_title(title)
+    # set on heroku CLI: heroku config:set OMDB_API_KEY='insert_key_here'
+    OMDB_API_KEY = ENV['OMDB_API_KEY']
     request = "http://www.omdbapi.com/?apikey=#{OMDB_API_KEY}&t=#{str}"
     response = HTTParty.get("#{request}").parsed_response
     return response
