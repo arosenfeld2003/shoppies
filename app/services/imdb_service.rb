3b4d93b9ab9081e96ccd1c6f7aa07e81b66fc43a
Alex Rosenfeld <arosenfeld2003@mac.com>
2020-08-31 16:00:41 -0700

    fix variable bug in movie_by_title

--- app/services/imdb_service.rb
+++ app/services/imdb_service.rb
@@ -12,7 +12,7 @@ class ImdbService
   def get_movie_by_title(title)
     # set on heroku CLI: heroku config:set OMDB_API_KEY='insert_key_here'
     OMDB_API_KEY = ENV['OMDB_API_KEY']
-    request = "http://www.omdbapi.com/?apikey=#{OMDB_API_KEY}&t=#{str}"
+    request = "http://www.omdbapi.com/?apikey=#{OMDB_API_KEY}&t=#{title}"
     response = HTTParty.get("#{request}").parsed_response
     return response
   end
